d9f10ad950ce9631d0faa7d2106ada513986250e
srinusura2006 <104443087+srinusura2006@users.noreply.github.com>
2022-07-06 23:28:50 +0530

    B2B

---
+++ js/index.js
@@ -0,0 +1,7 @@
+﻿const server = require('./server.js');
+
+const PORT = process.env.PORT || 7500;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
---
+++ js/server.js
@@ -0,0 +1,9 @@
+﻿server.post('/new-order', (req, res) => {
+
+    // Just logging the body of the request
+    console.log(req.body, 'data from webhook call')
+
+    // Sending a 200 to show success
+    res.status(200).json({ message: "success" })
+
+})
